ff6ab92dc4dea9edbaf6add993930372616a02ba
hajin <hajin@synapsoft.co.kr>
2022-02-17 20:50:59 +0900

    issue/3_16 mock을 이용한 테스트 - 사용자 로그인

---
+++ src/mock/user/test/user_service.test.js
@@ -0,0 +1,9 @@
+const UserService = require('../user_service');
+const UserClient = require('../user_client');
+
+describe(('UserService'), () => {
+    let userService;
+    beforeEach(() => {
+        userService = new UserService();
+    })
+})
